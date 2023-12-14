6d8ee92a125487ccb3e08464ffcb3fc344edf24f
aulia-izzatul <148217027+aulia-izzatul@users.noreply.github.com>
2023-12-14 07:38:13 -0700

    Add files via upload

---
+++ repository.dart
@@ -0,0 +1,41 @@
+import 'package:crud_apps/db_helper/database_connections.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Repository {
+  late DatabaseConnection _databaseConnection;
+  Repository() {
+    _databaseConnection = DatabaseConnection();
+  }
+  static Database? _database;
+  Future<Database?> get database async {
+    if (_database != null) {
+      return _database;
+    } else {
+      _database = await _databaseConnection.setDatabase();
+      return _database;
+    }
+  }
+
+  insertData(table, data) async {
+    var connection = await database;
+    return await connection?.query(table);
+  }
+
+  readDataById(table, itemId) async {
+    var connection = await database;
+    return await connection?.query(table, where: 'id=?', whereArgs: [itemId]);
+  }
+
+  updateData(table, data) async {
+    var connection = await database;
+    return await connection
+        ?.update(table, data, where: 'id=?', whereArgs: [data['id']]);
+  }
+
+  deleteDataById(table, itemId) async {
+    var connection = await database;
+    return await connection?.rawDelete("delete from $table where id=$itemId");
+  }
+
+  readData(String s) {}
+}
